48fc837312110e6d639916253336aa572f44bd10
Sachin B A <sachinba.20@gmail.com>
2024-09-17 11:41:32 +0530

    First commit

---
+++ demo.js
@@ -0,0 +1,10 @@
+djfjsdj
+kdj
+console.log()[D"Hello World");
+
+for()[A[D[D[D[D[D
+[Alet sum=0;[B[D[D[D[D[D[Dlet i=0 ; i<10 ; i++){}[D
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[F[F[F[B[B[B[F[B[F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
+	sum+=i;
+}
