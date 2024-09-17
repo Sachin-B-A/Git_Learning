57bf13eb6ec4c5840dcee8f0e28b6a0adfbf7330
Sachin B A <sachinba.20@gmail.com>
2024-09-17 11:45:43 +0530

    After changes

--- demo.js
+++ demo.js
@@ -1,10 +1,1 @@
-djfjsdj
-kdj
-console.log()[D"Hello World");
-
-for()[A[D[D[D[D[D
-[Alet sum=0;[B[D[D[D[D[D[Dlet i=0 ; i<10 ; i++){}[D
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[F[F[F[B[B[B[F[B[F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
-	sum+=i;
-}
+console.log()[D"Hello world");
